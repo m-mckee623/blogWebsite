7dff37f53e4819a1ddbbfa25b47a2153f6ce149b
m-mckee623 <matthew.mckee.2018@gmail.com>
2020-01-18 17:19:24 +0000

    Connect to local MDB, compose posts ad render to home page

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //jshint esversion:6
 
+//Declare Packages
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -11,58 +13,80 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-let posts = [];
-
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+//Create and connect to local mongodb
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
-app.get("/", function(req,res){
+//Create schema
+const postSchema = {
+  title: String,
+  content: String
+};
+
+//Declare new collection
+const Post = mongoose.model("Post", postSchema);
+
+//Render the home page and find posts stored in the db.
+app.get("/",function(req,res){
+Post.find({}, function(err, posts){
   res.render("home", {
-    startingContent:homeStartingContent,
-    journalPosts:posts
-  });
+    startingContent: homeStartingContent,
+    posts: posts
+    });
+});
 
 });
 
-app.get("/about", function(req,res){
-  res.render("about", {aboutHeadingContent:aboutContent});
-});
-
-app.get("/contact", function(req,res){
-  res.render("contact", {contactHeadingContent:contactContent});
-});
-
-app.get("/compose", function(req,res){
+//Compose Page
+app.get("/compose", function(req, res){
   res.render("compose");
 });
 
-app.post("/compose",function(req,res){
-  const post = {
+app.post("/compose", function(req, res){
+
+  const post = new Post ({
     title: req.body.postTitle,
-    body: req.body.postBody
-  };
+    content: req.body.postBody
+  });
 
-posts.push(post);
+  post.save(function(err){
 
-res.redirect("/");
+    if (!err){
+
+      res.redirect("/");
+
+    }
+
+  });
 });
 
-app.get("/posts/:journalPostName", function(req, res){
-  const requestedTitle = _.lowerCase(req.params.journalPostName);
+
+app.get("/posts/:postName", function(req, res){
+  const requestedTitle = _.lowerCase(req.params.postName);
 
   posts.forEach(function(post){
     const storedTitle = _.lowerCase(post.title);
 
-  if(storedTitle === requestedTitle) {
-    res.render("post", {
-      title: post.title,
-      content: post.body
-    });
-     }
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    }
   });
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutHeadingContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactHeadingContent: contactContent});
 });
 
 app.listen(3000, function() {
